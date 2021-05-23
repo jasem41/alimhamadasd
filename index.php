5da4f8111461876283e6f48c308283bbe4c6da8b
jasem41 <shardnet.tk@gmail.com>
2021-05-24 00:04:35 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 
 $user =$_GET['user'];
 $url = "https://www.m1m1.xyz/offers/offertoro.php?user=".$user;
-//$url ="https://www.offertoro.com/ifr/show/22813/".$user ."/11822";
-//echo file_get_contents($url);
+
+echo file_get_contents($url);
 ?>
-
-
-<iframe src="https://www.offertoro.com/ifr/show/22813/<?php echo $user; ?>/11822" frameborder="0" width="100%" height="100%" ></iframe> 
 
 

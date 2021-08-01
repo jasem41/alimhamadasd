73dde10954afacbda54cbdbcbeefae7077201010
jasem41 <shardnet.tk@gmail.com>
2021-08-01 22:55:43 +0300

    Add files via upload

--- adgem.php
+++ adgem.php
@@ -4,19 +4,28 @@
 $user =$_GET['user'];
 $url = "https://www.m1m1.xyz/offers/adgem.php?user=".$user;
 
-Redirect($url,false);
- function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
+include 'code.php';
 
-    exit();
-}
+if ($code =="US" ||
+$code =="CA"||
+$code =="FR"||
+$code =="NL"){
 
 
+echo file_get_contents($url);
 
+}else{
 ?>
 
+دولتك محظورة 
+الرجاء ,تشغيل الفي بي ان
+على احدى الدول :
+فرنسا
+هولندا
+امريكا
+كندا
+<?php
+
+}
+?>
 

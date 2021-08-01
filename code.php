61ea3823a4d61a05a9b971cc6cadcec93e54c9af
jasem41 <shardnet.tk@gmail.com>
2021-08-01 23:32:27 +0300

    Add files via upload

--- code.php
+++ code.php
@@ -5,14 +5,7 @@ $ip = $_SERVER['REMOTE_ADDR'];
 
 echo $ip;
 
-$url5 ="http://ip-api.com/json/".$ip;
 
-$response =file_get_contents($url5);
-
-
-	$json = json_decode($response, true);
-	
-$code =$json['countryCode'];
 
 
 }

2c6b6cef003edaa9a9b1668aa51c247a512ba4de
jasem41 <shardnet.tk@gmail.com>
2021-08-01 23:28:09 +0300

    Add files via upload

--- code.php
+++ code.php
@@ -3,35 +3,17 @@
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
+echo $ip;
 
 $url5 ="http://ip-api.com/json/".$ip;
 
-$ch = curl_init();
-
-//setup curl options
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_URL, $url5);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-//make request
-$response = curl_exec($ch);
-
-if($response === false) {
-	//curl error occurred, handle it how you like
-	echo curl_error($ch);
-}
-
-//close the curl object
-curl_close($ch);
+$response =file_get_contents($url5);
 
 
-if($response !== false) {
 	$json = json_decode($response, true);
 	
 $code =$json['countryCode'];
-print_r($json) ;
+
 
 }
 
